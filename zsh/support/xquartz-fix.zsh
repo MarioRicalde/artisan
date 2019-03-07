5d46240508a3a466c38052b861b42bd86b513977
MarioRicalde <kuroir@gmail.com>
2019-03-06 22:43:22 -0600

    Refactoring + comments.

--- zsh/support/xquartz-fix.zsh
+++ zsh/support/xquartz-fix.zsh
@@ -7,40 +7,15 @@ _='
 │  Docs: https://git.io/fhx1B                                 License: GPL 3.0 │ 
 └──────────────────────────────────────────────────────────────────────────────┘
 '
-## USAGE
+# XQuartz: If using xterm exit and switch to iterm
 #
-### Without arguments (default 1 dir back)
+# XQuartz is apparently refusing to connect to our iTerm Session ( with Vim ), for clipboard sharing, unless you run xterm ONCE. Once you do that it works flawlessly.
+# With this we understand that:
 #
-# $ pwd
-# /your/path/to/some/dir/a
-# $ cdb
-# $ pwd
-# /your/path/to/some/dir
- 
-#### With arguments
-#
-# $ pwd
-# /your/path/to/some/dir/a
-# $ cdb 3
-# $ pwd
-# /your/path
- 
-# Thanks to https://gist.github.com/rafmagana/4040559
- 
-function cdb
-{
-  local TIMES=$1
-  local INITIAL=1
-  local BACK_DIR='../'
-  local DIRS
- 
-  if [ $# -eq 0 ]; then
-    DIRS[0]="${BACK_DIR}"
-  fi
- 
-  for ((i=INITIAL;i<=TIMES;i++)); do
-    DIRS=("${DIRS[@]}" "${BACK_DIR}")
-  done
- 
-  cd `printf "%s" "${DIRS[@]}"`
-}
+#   1. xterm needs to open once when XQuartz open, so do it and exit with our zshrc instead of overriding the defaults.
+#   2. You can run xterm from iTerm to fix sync if XQuartz opened on its own via iTerm’s processes.
+if [[ -v XTERM_VERSION ]]; then
+  sleep 0.1;
+  osascript -e 'activate application "iTerm"' &!;
+  exit;
+fi
