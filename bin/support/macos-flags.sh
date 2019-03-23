e35d4ac8615b992e1fcda8fee557f50942824bdf
MarioRicalde <kuroir@gmail.com>
2019-03-23 15:21:51 -0600

    implements unrecoverable falg for artisan-macos closes #5

--- bin/support/macos-flags.sh
+++ bin/support/macos-flags.sh
@@ -4,11 +4,13 @@
 
 arg_visual_mode='false'
 arg_backup_name='false'
+arg_unrecoverable='false'
 
-while getopts ":vp:" o; do
+while getopts ":vp:u" o; do
   case "${o}" in
     v) arg_visual_mode='true' ;;
     p) arg_backup_name=${OPTARG} ;;
+    u) arg_unrecoverable='true' ;;
     *) usage ;;
   esac
 done
